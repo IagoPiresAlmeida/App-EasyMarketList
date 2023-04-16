55bd97bdaf496286b0557c02cf8823e1a2fefede
Skagth <105684891+Skagth@users.noreply.github.com>
2023-04-16 16:30:07 -0300

    style

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   FlatList,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 const App = () => {
   const [compras, setCompras] = useState('');
@@ -30,7 +31,8 @@ const App = () => {
     <View style={styles.compraItemContainer}>
       <Text style={styles.compraItem}>{item.text}</Text>
       <TouchableOpacity onPress={() => handleDeleteCompra(item.id)}>
-        <Feather name="check" size={24} color="black" />
+        <FontAwesome name="check" size={30} color="#79DF68" style={{shadowColor:'black', shadowOpacity: 0.4,shadowOffset:{width:1,height:2}}}/>
+
       </TouchableOpacity>
     </View>
   );
@@ -42,7 +44,7 @@ const App = () => {
           style={styles.input}
           value={compras}
           onChangeText={setCompras}
-          placeholder="Adicione um novo item na lista de compras"
+          placeholder="Adicione um item na lista de compras"
         />
         <TouchableOpacity style={styles.addButton} onPress={handleAddCompra}>
           <Text style={styles.addButtonText}>+</Text>
@@ -61,31 +63,41 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "mediumslateblue"
   },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginHorizontal: 20,
+    marginHorizontal: 10,
     marginVertical: 10,
     marginTop: 50,
   },
   input: {
     flex: 1,
-    height: 40,
+    height: 43,
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginRight: 10,
+    backgroundColor: "white"
   },
   addButton: {
     backgroundColor: '#0080ff',
     borderRadius: 5,
     padding: 10,
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderRadius: 5,
+    color: 'green'
+
   },
   addButtonText: {
     color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
+    width: 25,
+    height: 25,
+    textAlign: 'center'
   },
   comprasList: {
     paddingHorizontal: 20,
@@ -96,11 +108,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    backgroundColor: 'white'
   },
   compraItem: {
     fontSize: 16,
-    flex: 1,
-    margin:30
+    flex: 1,    
   },
 });
 
